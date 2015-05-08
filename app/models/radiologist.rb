e065952473b1b248086b40d5a9f79bc43c94580e
I-Ta Tsai@MBPR <ittsai@gmail.com>
2015-05-08 10:44:08 +0800

    validate radiologist no with uniqueness.

--- app/models/radiologist.rb
+++ app/models/radiologist.rb
@@ -1,6 +1,6 @@
 class Radiologist < ActiveRecord::Base
   has_and_belongs_to_many :biopsies
 
-  validates :no, presence: true
+  validates :no, presence: true, uniqueness: true
   validates :name, presence: true
 end
